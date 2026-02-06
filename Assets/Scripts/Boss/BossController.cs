e4cbd4078233d545d9c4bb78ad555bbb0747e7a1
Kefi <ablasmarti@gmail.com>
2026-02-06 23:14:25 +0100

    Sandersilla polilla

---
+++ Assets/Scripts/Boss/BossController.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    private int vidaActual;
+    [SerializeField] private int vidaMaxima = 3;
+    private enum EstadoBoss {Idle, Atacando, Muerto}
+    private EstadoBoss estadoActual;
+
+    [SerializeField] private float tiempoEntreAtaques = 2f;
+    private float tiempoUltimoAtaque;
+
+    [SerializeField] private TentacleAttack tentaculoIzquierdo;
+    [SerializeField] private TentacleAttack tentaculoDerecho;
+    [SerializeField] private EyeController ojo;
+    [SerializeField] private LaserRain lanzadorLaseres;
+
+    void Start()
+    {
+        vidaActual = vidaMaxima;
+        estadoActual = EstadoBoss.Idle;
+        tiempoUltimoAtaque = -tiempoEntreAtaques;
+    }
+
+    void Update()
+    {
+        if (estadoActual == EstadoBoss.Idle && Time.time - tiempoUltimoAtaque >= tiempoEntreAtaques)
+        {
+            SeleccionarAtaqueAleatorio();
+            estadoActual = EstadoBoss.Atacando;
+            tiempoUltimoAtaque = Time.time;
+        }
+    }
+
+void SeleccionarAtaqueAleatorio()
+{
+    int ataqueAleatorio = Random.Range(0, 3);
+    
+    if (ataqueAleatorio == 0)
+    {
+        EjecutarAtaqueTentaculo(Random.Range(0, 2) == 0);
+    }
+    else if (ataqueAleatorio == 1)
+    {
+        EjecutarAtaqueLluvia();
+    }
+    else
+    {
+        EjecutarAtaqueOjo();
+    }
+}
+
+void EjecutarAtaqueTentaculo(bool esIzquierdo)
+{
+    if (esIzquierdo)
+    {
+        tentaculoIzquierdo.IniciarBarrido();
+    }
+    else
+    {
+        tentaculoDerecho.IniciarBarrido();
+    }
+}
+
+void EjecutarAtaqueLluvia()
+{
+    lanzadorLaseres.IniciarAtaque();
+}
+
+void EjecutarAtaqueOjo()
+{
+    ojo.IniciarAtaque();
+}
+
+public void RecibirDaño()
+{
+    vidaActual--;
+    
+    if (vidaActual <= 0)
+    {
+        Morir();
+    }
+}
+
+void Morir()
+{
+    estadoActual = EstadoBoss.Muerto;
+    gameObject.SetActive(false);
+}
+
+public void FinalizarAtaque()
+{
+    estadoActual = EstadoBoss.Idle;
+}
+
+}
