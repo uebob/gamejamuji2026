062800120d6b125d731365bb9f2f915411d3737f
uebo <jamarag2006@gmail.com>
2026-02-06 21:28:27 +0100

    actualizado input handler
    apoya both en lugar de solo el nuevo

---
+++ Assets/Scripts/player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+}
