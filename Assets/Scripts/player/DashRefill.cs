0e6da25ca4359c9d7206397862c986d659629d8c
uebo <jamarag2006@gmail.com>
2026-02-06 22:16:52 +0100

    added dash and dash refill

---
+++ Assets/Scripts/player/DashRefill.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class DashRefill : MonoBehaviour
+{
+    private Collider2D col;
+    private float noColFrames = 0.5f;
+
+    private void Start()
+    {
+        col = GetComponent<Collider2D>();
+        col.enabled = false;
+        StartCoroutine(EnableCollider());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            collider.gameObject.GetComponent<PlayerMovement>().canDash = true;
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator EnableCollider()
+    {
+        yield return new WaitForSeconds(noColFrames);
+        col.enabled = true;
+    }
+}
--- Assets/Scripts/player/PlayerMovement.cs
+++ Assets/Scripts/player/PlayerMovement.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
+using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float acceleration = 20f; 
+    public float acceleration = 20f;
+
+    [SerializeField] private float dashSpeed = 20f;
+    [SerializeField] private float dashAcceleration = 60f;
+    [SerializeField] private float dashDeceleration = 100f;
+    [SerializeField] private float dashDuration = 0.2f;
+    [SerializeField] public bool canDash = true;
+
+    public GameObject refillPrefab;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
+
+    private bool isDashing = false;
+    private float lastDashTime = -100f;
 
     private void Awake()
     {
@@ -18,11 +31,47 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveInput = new Vector2(moveX, moveY).normalized;
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isDashing)
+        {
+            if (canDash)
+            {
+                if (moveInput != Vector2.zero)
+                {
+                    canDash = false;
+                    StartCoroutine(DashRoutine());
+                }
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        Vector2 targetVelocity = moveInput * moveSpeed;
-        rb.linearVelocity = Vector2.MoveTowards(rb.linearVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+        float currentSpeed = isDashing ? dashSpeed : moveSpeed;
+        float currentAcceleration = acceleration;
+
+        if (isDashing)
+        {
+            currentAcceleration = dashAcceleration;
+        }
+        else if (rb.linearVelocity.magnitude > moveSpeed + 0.5f)
+        {
+            currentAcceleration = dashDeceleration;
+        }
+
+        Vector2 targetVelocity = moveInput * currentSpeed;
+
+        rb.linearVelocity = Vector2.MoveTowards(rb.linearVelocity, targetVelocity, currentAcceleration * Time.fixedDeltaTime);
+    }
+
+    private IEnumerator DashRoutine()
+    {
+        isDashing = true;
+        lastDashTime = Time.time;
+        Instantiate(refillPrefab, transform.position, transform.rotation);
+
+        yield return new WaitForSeconds(dashDuration);
+
+        isDashing = false;
     }
 }
